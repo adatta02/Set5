2b6ef66e08f63b738ccd8f4afe46cc9efe011d08
Phillip Booth <phillip.booth2015@gmail.com>
2015-06-11 14:59:17 -0400

    adding FOS user Bundle

--- symfony_text_app/src/AppBundle/Entity/User.php
+++ symfony_text_app/src/AppBundle/Entity/User.php
@@ -2,6 +2,7 @@
 // src/AppBundle/Entity/Product.php
 namespace AppBundle\Entity;
 
+use FOS\UserBundle\Model\User as BaseUser;
 use Doctrine\ORM\Mapping as ORM;
 use Symfony\Component\Validator\Constraints as Assert;
 
@@ -9,149 +10,16 @@ use Symfony\Component\Validator\Constraints as Assert;
  * @ORM\Entity()
  * @ORM\Table(name="user")
  */
-class User
+class User extends BaseUser
 {
      /**
      * @ORM\Column(type="integer")
      * @ORM\Id
      * @ORM\GeneratedValue(strategy="AUTO")
      */
-    private $id;
+    protected $id;
 
-    /**
-     * @ORM\Column(type="string", length=30)
-     * @Assert\NotBlank()
-     * @Assert\NotNull()
-     * @Assert\Type(
-     *     type="string",
-     *     message="The value {{ value }} is not a valid {{ type }}."
-     * )
-     *
-     * @Assert\Length(
-     *      min = 3,
-     *      max = 30,
-     *      minMessage = "Your first name must be at least {{ limit }} characters long",
-     *      maxMessage = "Your first name cannot be longer than {{ limit }} characters"
-     * )
-     *
-     */
-    private $userName;
-
-    /**
-     * @ORM\Column(type="string", length=30)
-     * @Assert\NotBlank()
-     * @Assert\NotNull()
-     * @Assert\Type(
-     *     type="string",
-     *     message="The value {{ value }} is not a valid {{ type }}."
-     * )
-     * @Assert\Length(
-     *      min = 3,
-     *      max = 30,
-     *      minMessage = "Your Email must be at least {{ limit }} characters long",
-     *      maxMessage = "Your Email cannot be longer than {{ limit }} characters"
-     * )
-     * @Assert\Email(
-     *     message = "The email '{{ value }}' is not a valid email.",
-     *     checkMX = true
-     * )
-     */
-    private $email;
-
-    /**
-     * @ORM\Column(type="string", length=30)
-     * @Assert\NotBlank()
-     * @Assert\NotNull()
-     * @Assert\Type(
-     *     type="string",
-     *     message="The value {{ value }} is not a valid {{ type }}."
-     * )
-     * @Assert\Length(
-     *      min = 3,
-     *      max = 30,
-     *      minMessage = "Your password must be at least {{ limit }} characters long",
-     *      maxMessage = "Your password cannot be longer than {{ limit }} characters"
-     * )
-     */
-    private $password;
-
-
-    /**
-     * Get id
-     *
-     * @return integer 
-     */
-    public function getId()
-    {
-        return $this->id;
-    }
-
-    /**
-     * Set userName
-     *
-     * @param string $userName
-     * @return User
-     */
-    public function setUserName($userName)
-    {
-        $this->userName = $userName;
-
-        return $this;
-    }
-
-    /**
-     * Get userName
-     *
-     * @return string 
-     */
-    public function getUserName()
-    {
-        return $this->userName;
-    }
-
-    /**
-     * Set email
-     *
-     * @param string $email
-     * @return User
-     */
-    public function setEmail($email)
-    {
-        $this->email = $email;
-
-        return $this;
-    }
-
-    /**
-     * Get email
-     *
-     * @return string 
-     */
-    public function getEmail()
-    {
-        return $this->email;
-    }
-
-    /**
-     * Set password
-     *
-     * @param string $password
-     * @return User
-     */
-    public function setPassword($password)
-    {
-        $this->password = $password;
-
-        return $this;
-    }
-
-    /**
-     * Get password
-     *
-     * @return string 
-     */
-    public function getPassword()
-    {
-        return $this->password;
-    }
 }
+
+
+
